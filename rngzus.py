4e5d5f56a8bcd8d45f03da42b65591a5ba6ec9c2
Ryan Fleming <ryan@soprisapps.com>
2022-04-04 18:06:51 -0600

    add the bot

---
+++ rngzus.py
@@ -0,0 +1,49 @@
+import os
+import random
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+
+@bot.command(name='99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+@bot.command(name='rngzus', help='takes a comma sepearted list of users and will choose 1 to win a give away')
+async def give_away(ctx, list_of_users: str):
+    arr = list_of_users.split(',')
+    response = random.choice(arr)
+    await ctx.send(response)
+
+
+bot.run(TOKEN)
